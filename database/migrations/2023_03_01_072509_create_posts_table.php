aa86b381a2f34cbb85f6cd8beeca51cf6a847bda
Wldyn307 <waliyudinhelmi307@gmail.com>
2023-03-01 21:53:38 +0700

    index crud

--- database/migrations/2023_03_01_072509_create_posts_table.php
+++ database/migrations/2023_03_01_072509_create_posts_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->string('content');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
